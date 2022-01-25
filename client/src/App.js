b5d96242f5f71c7a9c4d3ac1ebd7b8e49167c803
sarit-chaet-hudis <sarit.chh@gmail.com>
2022-01-25 15:26:34 +0200

    matter js test

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Comp from "./Components/Comp";
+import Scene from "./Components/Scene";
+
+function App() {
+  return (
+    <div className="App">
+      smthng?
+      <Scene />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Comp2.jsx
@@ -0,0 +1,5 @@
+// import { Engine, Render, Bodies, World, Matter } from "matter-js";
+
+// function Comp2 (props) {
+
+// }
